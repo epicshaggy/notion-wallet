d42544e5f60d21fc9817dce55140474243d16d96
Jose Martinez <epicshaggy@gmail.com>
2022-08-12 12:22:15 -0400

    Prompt for token with alert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { Expense } from '../models/expense.model';
 import { NotionService } from '../notion.service';
 
@@ -10,12 +11,64 @@ import { NotionService } from '../notion.service';
 export class HomePage implements OnInit {
   isLoading = true;
   expenses: Expense[] = [];
-  constructor(private notionService: NotionService) {}
 
-  ngOnInit(): void {
+  constructor(
+    private notionService: NotionService,
+    private alertController: AlertController
+  ) {}
+
+  async ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.log(await this.promptFortoken());
+      return;
+    }
+
     this.notionService.getExpenses().subscribe((expenses: Expense[]) => {
       this.expenses = expenses;
       this.isLoading = false;
     });
+  }
+
+  async promptFortoken() {
+    const a = await this.alertController.create({
+      header: 'Notion Token',
+      message: 'Please enter your Notion token',
+      inputs: [
+        {
+          name: 'token',
+          type: 'text',
+          placeholder: 'Notion Token',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'OK',
+        },
+      ],
+    });
+
+    a.present();
+    const { role, data } = await a.onDidDismiss();
+
+    if (role === 'cancel') {
+      return false;
+    }
+
+    if (!!data?.values?.token) {
+      localStorage.setItem('token', data.values.token);
+      return true;
+    }
+
+    return false;
+  }
+
+  changeToken() {
+    this.promptFortoken();
   }
 }
